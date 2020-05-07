5efcf42c342bdb9eec4e5a2995389b949e4dfad8
Izuking <matthiasogbodo@gmail.com>
2020-05-07 15:48:03 +0100

    add chat model

---
+++ src/models/chat.ts
@@ -0,0 +1,26 @@
+import mongoose, { Schema, Document } from 'mongoose';
+
+export interface IChat extends Document {
+  userId: string;
+  recipientId: string;
+  mediaURL?: string;
+  content?: string;
+}
+
+const ChatModelSchema = new Schema(
+  {
+    userId: { type: Schema.Types.ObjectId, ref: 'User', required: true },
+    recipientId: {
+      type: Schema.Types.ObjectId,
+      ref: 'Recipient',
+      required: true,
+    },
+    mediaURL: { type: String, required: true },
+    content: { type: String, required: true },
+  },
+  { timestamps: true },
+);
+
+const ChatModel = mongoose.model<IChat>('Chat', ChatModelSchema);
+
+export default ChatModel;
