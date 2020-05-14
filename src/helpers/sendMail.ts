5bc4ea8ebf6a748d656050aaf478da97a62431d6
jagajastic <josephibrahi@gmail.com>
2020-05-14 14:47:30 +0100

    add sendmail function

---
+++ src/helpers/sendMail.ts
@@ -0,0 +1,29 @@
+import * as nodemailer from 'nodemailer';
+
+console.log(nodemailer);
+
+// var nodemailer: any = require("nodemailer");
+
+const sendMail = (email: string, message: string, subject: string) => {
+  const smtpTransport = nodemailer.createTransport({
+    host: 'smtp.gmail.com',
+    port: 465,
+    secure: true,
+    auth: {
+      user: process.env.MAIL_USER,
+      pass: process.env.MAIL_PASSWORD,
+    },
+  });
+  let mailOptions = {
+    to: email,
+    from: process.env.FROM_EMAIL,
+    subject,
+    html: message,
+  };
+  smtpTransport.sendMail(mailOptions, (err: any) => {
+    if (err) return new Error(err);
+    return;
+  });
+};
+
+export default sendMail;
