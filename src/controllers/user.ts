781f92e298ff5a7da149523c2adeaccc94760ea6
jagajastic <josephibrahi@gmail.com>
2020-05-16 15:32:47 +0100

    restrucuture response in user controller

--- src/controllers/user.ts
+++ src/controllers/user.ts
@@ -1,46 +1,74 @@
 import { Request, Response } from 'express';
 import { allUsers, getUser, saveUser, updateUserDetails } from '../API/user';
 import response from '../helpers/response';
-import { IUser } from '../models/user';
 import httpStatus from 'http-status';
 import { tokenEncoder } from '../helpers/tokenEncoder';
 import messages from '../helpers/mailMessage';
 import sendMail from '../helpers/sendMail';
 
 // get all users
-export const getAlluser = async (_req: Request, res: Response) => {
-  const userDatas = await allUsers();
-  return res.json(response(httpStatus.OK, 'success', userDatas, ''));
+export const getAllUser = async (_req: Request, res: Response) => {
+  const userData = await allUsers();
+  return res.json(
+    response({
+      statusCode: httpStatus.OK,
+      message: 'success',
+      payload: userData,
+    }),
+  );
 };
 
 // get a user
 export const getAUserByEmail = async (req: Request, res: Response) => {
   try {
-    const userData: IUser | null = await getUser(req.params);
+    const userData = await getUser(req.params);
     if (!userData) {
       return res.json(
-        response(httpStatus.NOT_FOUND, 'user not found', {}, null, ''),
+        response({
+          statusCode: httpStatus.NOT_FOUND,
+          message: 'user not found',
+          payload: {},
+        }),
       );
     }
     return res.json(
-      response(httpStatus.OK, 'Success', userData || {}, null, ''),
+      response({
+        statusCode: httpStatus.OK,
+        message: 'Success',
+        payload: userData,
+      }),
     );
   } catch (error) {
+    if (error.code) {
+      return res.json(response({ ...error }));
+    }
     throw new Error(error);
   }
 };
+
 export const getAUserById = async (req: Request, res: Response) => {
   try {
-    const userData: IUser | null = await getUser(req.params);
+    const userData = await getUser(req.params);
     if (!userData) {
       return res.json(
-        response(httpStatus.NOT_FOUND, 'user not found', {}, null, ''),
+        response({
+          statusCode: httpStatus.NOT_FOUND,
+          message: 'user not found',
+          payload: {},
+        }),
       );
     }
     return res.json(
-      response(httpStatus.OK, 'Success', userData || {}, null, ''),
+      response({
+        statusCode: httpStatus.OK,
+        message: 'Success',
+        payload: userData,
+      }),
     );
   } catch (error) {
+    if (error.code) {
+      return res.json(response({ ...error }));
+    }
     throw new Error(error);
   }
 };
@@ -49,25 +77,23 @@ export const getAUserById = async (req: Request, res: Response) => {
 export const createUser = async (req: Request, res: Response) => {
   try {
     const { email } = req.body;
-    // check if email exist
-    const isExist = await getUser({ email });
-    if (!isExist) {
-      const result = await saveUser(req.body);
-      // send email after signup
-      const subject =
-        'Welcome to Fansunity! 👋 Please confirm your email address';
-      const token = tokenEncoder(email, result._id, false);
-      await sendMail(email, messages.confirmationEmail(token), subject);
-
-      return res.json(
-        response(httpStatus.OK, 'Success', result || {}, null, ''),
-      );
-    }
-
+    const result = await saveUser(req.body);
+    // send email after signup
+    const subject =
+      'Welcome to Fansunity! 👋 Please confirm your email address';
+    const token = tokenEncoder(email, result._id, false);
+    sendMail(email, messages.confirmationEmail(token), subject);
     return res.json(
-      response(httpStatus.CONFLICT, 'Email Already Exist', {}, null, ''),
+      response({
+        statusCode: httpStatus.OK,
+        message: 'Success',
+        payload: result,
+      }),
     );
   } catch (error) {
+    if (error.code) {
+      return res.json(response({ ...error }));
+    }
     throw new Error(error);
   }
 };
@@ -76,27 +102,30 @@ export const createUser = async (req: Request, res: Response) => {
 export const updateUserInfo = async (req: Request, res: Response) => {
   try {
     // check user by id if he/she exist
-    const isExist = getUser({ _id: req.params.id });
-    if (isExist) {
-      const updatedUserData = await updateUserDetails({
-        _id: req.params.id,
-        body: req.body,
-      });
-
+    const updatedUserData = await updateUserDetails({
+      _id: req.params.id,
+      body: req.body,
+    });
+    if (!updatedUserData) {
       return res.json(
-        response(
-          httpStatus.OK,
-          'account information updated',
-          updatedUserData || {},
-          '',
-        ),
+        response({
+          statusCode: httpStatus.NOT_FOUND,
+          message: 'user not found',
+          payload: {},
+        }),
       );
     }
-
     return res.json(
-      response(httpStatus.NOT_FOUND, 'user not found', {}, null, ''),
+      response({
+        statusCode: httpStatus.OK,
+        message: 'account information updated',
+        payload: updatedUserData,
+      }),
     );
   } catch (error) {
+    if (error.code) {
+      return res.json(response({ ...error }));
+    }
     throw new Error(error);
   }
 };
@@ -105,22 +134,32 @@ export const updateUserInfo = async (req: Request, res: Response) => {
 export const deleteUser = async (req: Request, res: Response) => {
   try {
     // check if user exist
-    const isExist = getUser({ _id: req.params._id });
-    if (isExist) {
-      const updatedUserData = await updateUserDetails({
-        _id: req.params.id,
-        body: req.body,
-      });
+    const deletedUserData = await updateUserDetails({
+      _id: req.params.id,
+      body: req.body,
+    });
 
+    if (!deletedUserData) {
       return res.json(
-        response(httpStatus.OK, 'account deleted', updatedUserData || {}, ''),
+        response({
+          statusCode: httpStatus.NOT_FOUND,
+          message: 'user not found',
+          payload: {},
+        }),
       );
     }
 
     return res.json(
-      response(httpStatus.NOT_FOUND, 'user not found', {}, null, ''),
+      response({
+        statusCode: httpStatus.OK,
+        message: 'account deleted',
+        payload: deletedUserData,
+      }),
     );
   } catch (error) {
+    if (error.code) {
+      return res.json(response({ ...error }));
+    }
     throw new Error(error);
   }
 };
