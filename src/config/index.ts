d97cfce5ad75f77c3b0707fb3b2fee5b383238d5
jagajastic <josephibrahi@gmail.com>
2020-06-11 11:23:26 +0100

    root config

---
+++ src/config/index.ts
@@ -0,0 +1,36 @@
+import { merge } from 'lodash';
+const env = process.env.NODE_ENV || 'development';
+
+interface IConfig {
+  [key: string]: any;
+}
+
+// default if all other db setup is not giving or defined
+const baseConfig = {
+  env,
+  isDev: env === 'development',
+  isTest: env === 'testing',
+  port: 3000,
+  secrets: {
+    jwt: process.env.JWT_SECRET,
+    jwtExp: '100d',
+  },
+};
+
+// environmental configuration
+let envConfig = {};
+
+switch (env) {
+  case 'dev':
+  case 'development':
+    envConfig = require('./dev').config;
+    break;
+  case 'test':
+  case 'testing':
+    envConfig = require('./testing').config;
+    break;
+  default:
+    envConfig = require('./dev').config;
+}
+
+export default merge(baseConfig, envConfig) as IConfig;
